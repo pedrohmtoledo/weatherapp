b14f8880c5a6f415d883583d3b9faa9a18c6242c
pedrohmtoledo <toledopedro.hmt@gmail.com>
2025-02-11 18:15:05 +0000

    Fix temp toggle button

--- src/modules/tempformat.js
+++ src/modules/tempformat.js
@@ -4,19 +4,18 @@ export function temperatureToggle() {
   const fahrenheitText = document.querySelector('.switch-f');
   let temp;
 
-  if (fahrenheitText.style.opacity != 1) {
-    slider.style.transform = 'translateX(36px)';
+  if (getComputedStyle(fahrenheitText).opacity === '0') {
+    slider.style.transform = 'translateX(0px)';
     slider.style.background = '#ff6384';
     celsiusText.style.opacity = '0';
     fahrenheitText.style.opacity = '1';
     temp = 'farenheit';
   } else {
-    slider.style.transform = 'translateX(0px)';
+    slider.style.transform = 'translateX(36px)';
     slider.style.background = '#6c63ff';
     celsiusText.style.opacity = '1';
     fahrenheitText.style.opacity = '0';
     temp = 'celcius';
   }
   localStorage.setItem('temp', JSON.stringify(temp));
-  return temp;
 }
